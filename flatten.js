b7efc39c1edebb7dacf931d6fa2395780bcfcef1
Roman Gorelik <romang31@gmail.com>
2019-02-27 12:26:02 -0500

    flatten function solved

---
+++ flatten.js
@@ -0,0 +1,14 @@
+// Write a recursive function which accepts an array or arrays and 
+// returns a new array with all values flattened.
+
+let flatten = (arr) => {
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flatten(arr[i]))
+    } else {
+      result.push(arr[i])
+    }
+  }
+  return result
+}
