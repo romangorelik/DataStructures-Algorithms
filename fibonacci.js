ab3a07d68c6354e0636c00b2c8a30407930e452e
Roman Gorelik <romang31@gmail.com>
2019-02-27 12:00:36 -0500

    fibonacci function solved

---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+// Write a recursive function which accepts a number and returns the nth number
+// in the Fibonacci sequence. Recall that the Fibonacci sequence is the sequence
+// if whole numbers 1, 1, 2, 3, 5, 8... which starts with 1 and 1, and where every
+// number thereafter is equal to the sum of the previous two numbers
+
+let fib = (num) => {
+  if (num <= 2) return 1
+  return fib(num - 1) + fib(num - 2)
+}
+
