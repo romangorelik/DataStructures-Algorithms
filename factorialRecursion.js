05aa98b9d815e8a63e0954c204eafbd1f68a9837
Roman Gorelik <romang31@gmail.com>
2019-02-27 11:43:23 -0500

    added another base case for factorial with 0

--- factorialRecursion.js
+++ factorialRecursion.js
@@ -1,6 +1,7 @@
 // Write a recursive function to find the factorial of the number provided.
 
 let factorialRecursion = (num) => {
+  if (num === 0) return 1
   if (num === 1) return 1
   return num * factorialRecursion(num - 1)
 }
