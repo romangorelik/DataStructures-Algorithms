12c6a3f03cc96aadb7ff8d3f8a7b3e47ad173d6b
Roman Gorelik <romang31@gmail.com>
2019-03-04 15:05:32 -0500

    bubbleSort function optimized

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,16 +1,21 @@
 // Write a function that implements bubble sort, and returns a sorted array.
 
 let bubbleSort = (arr) => {
+  let noSwaps;
+
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j]
         arr[j] = arr[j + 1]
         arr[j + 1] = temp
+        noSwaps = false
       }
     }
+    if (noSwaps) break
   }
   return arr
 }
 
-console.log(bubbleSort([5, 1, 3, 4, 2]))
+// Added a check to see if no swaps where made on the previous run, to short circuit the sorting.
