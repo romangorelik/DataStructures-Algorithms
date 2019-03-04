111bb677bc9c4f66d31c90182b82187d4c2f6d45
Roman Gorelik <romang31@gmail.com>
2019-03-04 13:59:09 -0500

    bubbleSort function solved

---
+++ bubbleSort.js
@@ -0,0 +1,16 @@
+// Write a function that implements bubble sort, and returns a sorted array.
+
+let bubbleSort = (arr) => {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+console.log(bubbleSort([5, 1, 3, 4, 2]))
